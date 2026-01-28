f9d7644f964f0d6622f76f1f2e4ed87d1736343c
Kryakn <aryanshrivastav150@gmail.com>
2026-01-29 00:38:02 +0530

    practice patterns

---
+++ 6_pattern.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// 1                 1 
+// 1 2             2 1 
+// 1 2 3         3 2 1 
+// 1 2 3 4     4 3 2 1 
+// 1 2 3 4 5 5 4 3 2 1 
+// 1 2 3 4     4 3 2 1 
+// 1 2 3         3 2 1 
+// 1 2             2 1 
+// 1                 1 
