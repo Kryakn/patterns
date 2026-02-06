d25d3cdd299e47100df65c10369a601a5e061502
Kryakn <aryanshrivastav150@gmail.com>
2026-02-06 13:31:37 +0530

    patterns

--- 31_pattern.cpp
+++ 31_pattern.cpp
@@ -6,12 +6,12 @@ int main(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
 
-           if(j%2==0){
-            cout<<"* ";
+           if(i%2==1){
+            cout<<i<<" ";
            }
            else
            {
-            cout<<i<<" ";
+            cout<<"*  ";
 
            }
             
