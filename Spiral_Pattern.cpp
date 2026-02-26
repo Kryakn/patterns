4f513a31d838e1d6e637e1480c9c3cf60d0e3937
Kryakn <aryanshrivastav150@gmail.com>
2026-02-26 15:52:59 +0530

    Spiral Pattern

--- Spiral_Pattern.cpp
+++ Spiral_Pattern.cpp
@@ -1,10 +1,11 @@
+//spiral form
 #include<iostream>
 #include<vector>
 using namespace std;
 int main (){
     int r,c;
     cin>>r>>c;
-    vector<vector<int>>arr(r,vector<int>(c));
+    vector<vector<int>>arr(r,vector<int>(c));//initialized 2d vector
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             cin>>arr[i][j];
