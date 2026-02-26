3ed61200556bb19880de7f6225d02d32b466a4da
Kryakn <aryanshrivastav150@gmail.com>
2026-02-26 15:19:48 +0530

    Wave form pattern

---
+++ Wave_Form_Pattern.cpp
@@ -0,0 +1,28 @@
+//WAVE FORM TRAVERSING
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+int n,m;
+//n=row,m=column
+cin>>m>>n;
+vector<vector<int>>a(n,vector<int>(m));
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+     }
+     for(int j=0;j<m;j++){
+        if(j%2==0){
+            for(int i=0;i<n;i++){
+                cout<<a[i][j]<<"  ";
+            }
+        }
+        else{
+            for(int i=n-1;i>=0;i--){
+                cout<<a[i][j]<<"  ";
+            }
+        }
+     }
+    return 0;
+}
